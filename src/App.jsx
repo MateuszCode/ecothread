1fe57ac2881a82686ed7c11eb0bf912a4f23a9e5
cybertecna <mjkopacki@gmail.com>
2024-02-20 13:05:12 +0100

    Created the footer and header with basic styling.

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Shop from "./pages/Shop"
+import Contact from "./pages/Contact"
+import { TfiInstagram } from "react-icons/tfi";
+import { TfiFacebook } from "react-icons/tfi";
+import { TfiTwitter } from "react-icons/tfi";
+
+
+export default function App() {
+    return (
+          <BrowserRouter>
+            <header>
+                <h1 className="name-logo">ECOTHREAD</h1>
+                <nav>
+                    <Link to="/" className='navigation-link'>Home</Link>
+                    <Link to="/shop" className='navigation-link'>Shop</Link>
+                    <Link to="/about" className='navigation-link'>About Us</Link>
+                    <Link to="/contact" className='navigation-link'>Contact</Link>
+                </nav>
+                <div className="login">
+                  <Link className='login-link'>Login</Link>
+                  <Link className='register-link'>Register</Link>
+                </div>
+            </header>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/contact" element={<Contact />}>Contact</Route>
+            </Routes>
+            <footer>
+              <h1 className="name-logo-footer">ECOTHREAD</h1>
+              <div className="nav-footer">
+                    <Link to="/" className='navigation-link-footer'>Home</Link>
+                    <Link to="/shop" className='navigation-link-footer'>Shop</Link>
+                    <Link to="/about" className='navigation-link-footer'>About Us</Link>
+                    <Link to="/contact" className='navigation-link-footer'>Contact</Link>
+              </div>
+              <div className="copyright-socials">
+                <p className="copyright">© 2024 Ecothread, All Rights Reserved.</p>
+                <div className="social-media">
+                <a><TfiInstagram className="socials-icon"/></a>
+                <a><TfiFacebook className="socials-icon"/></a>
+                <a><TfiTwitter className="socials-icon" /></a>
+
+                </div>
+
+              </div>
+              
+            </footer>
+          </BrowserRouter>
+        )
+      
+}
