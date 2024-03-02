f26103d944d6c2d902b2b747fc5d753d2b308bf0
cybertecna <mjkopacki@gmail.com>
2024-03-02 13:56:27 +0100

    The total item price will be displayed up to 2 decimals.

--- Components/CartItem.jsx
+++ Components/CartItem.jsx
@@ -30,7 +30,7 @@ export default function CartItem({productId, quantity}) {
                 <p className="cart-product-text"><span style={{fontWeight:"bold", color:"rgb(53, 69, 43)"}}>Price:</span> {product.price}$ </p>
                 <p className="cart-product-text" ><span style={{fontWeight:"bold", color:"rgb(53, 69, 43)"}}>Quantity:</span> {quantity}</p>
                 {product.price * quantity === product.price ? null :
-                <p className="cart-product-text"><span style={{fontWeight:"bold", color:"rgb(53, 69, 43)"}}>Total:</span> {product.price * quantity}$</p>
+                <p className="cart-product-text"><span style={{fontWeight:"bold", color:"rgb(53, 69, 43)"}}>Total:</span> {Math.round(product.price * quantity)/100}$</p>
                 }
                 <RemoveButton productId={productId}></RemoveButton>
 
